9392d9d7091cea20b77fba3ca142b8ac9b1f45f1
VisualizedConfusion2 <thh005.edu.zealand.dk>
2025-04-08 10:09:29 +0200

    update

--- Codechallenge_v6/Program.cs
+++ Codechallenge_v6/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
+            List<string> Ingredienser = new List<string>();
+            Ingredienser.Add("g");
+            Ingredienser.Add("g");
+            Ingredienser.Add("g");
+            Ingredienser.Add("g");
+            Ingredienser.Add("g");
+            Ingredienser.Add("g");
+            Ingredienser.Add("g");
+            Ingredienser.Add("g");
+            Ingredienser.Add("g");
+            Ingredienser.Add("g");
+            Ingredienser.Add("g");
+            Ingredienser.Add("g");
+            Ingredienser.Add("g");
+
+
+            foreach (string ingrediens in Ingredienser)
+            {
+                Console.WriteLine($"-ingrediens");
+            }
         }
     }
 }
-            Console.WriteLine("Hello, World!");
