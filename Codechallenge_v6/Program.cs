2198f42e4a42e3c1a8b9e2b146b2c7cbc9363902
VisualizedConfusion2 <thh005.edu.zealand.dk>
2025-04-08 10:12:59 +0200

    gg

--- Codechallenge_v6/Program.cs
+++ Codechallenge_v6/Program.cs
@@ -5,19 +5,15 @@
         static void Main(string[] args)
         {
             List<string> Ingredienser = new List<string>();
-            Ingredienser.Add("g");
-            Ingredienser.Add("g");
-            Ingredienser.Add("g");
-            Ingredienser.Add("g");
-            Ingredienser.Add("g");
-            Ingredienser.Add("g");
-            Ingredienser.Add("g");
-            Ingredienser.Add("g");
-            Ingredienser.Add("g");
-            Ingredienser.Add("g");
-            Ingredienser.Add("g");
-            Ingredienser.Add("g");
-            Ingredienser.Add("g");
+            Ingredienser.Add("200g hvedemel");
+            Ingredienser.Add("2 teskefuld Bagepulver");
+            Ingredienser.Add("½ teskefuld fint salt");
+            Ingredienser.Add("2 æg");
+            Ingredienser.Add("150g sukker");
+            Ingredienser.Add("1 teskefuld vaniljesukker");
+            Ingredienser.Add("125g smeltet smør");
+            Ingredienser.Add("2 modne bananer");
+            Ingredienser.Add("100g mørk chokolade");
 
 
             foreach (string ingrediens in Ingredienser)
